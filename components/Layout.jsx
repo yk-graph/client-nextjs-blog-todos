bde15eec4ea0c18497707a6abce8b0974460daa2
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-18 07:19:03 +0900

    initial commit

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Layout({ children, title = 'default title' }) {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex-1 justify-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+        @udemy 2021
+      </footer>
+    </div>
+  )
+}
