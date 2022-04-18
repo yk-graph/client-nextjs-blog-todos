2c6e24814a9163208fa7161817e3ca127b83625e
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-18 12:02:40 +0900

    feat: main-pageの作成、メニューボタン設置

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import Cookies from 'universal-cookie'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function MainPage() {
   const router = useRouter()
@@ -16,6 +17,18 @@ export default function MainPage() {
 
   return (
     <Layout title="Main Page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8  hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Blog by SSG + ISR
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 ml-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            Visit Task by ISR + CSR
+          </a>
+        </Link>
+      </div>
       <svg
         onClick={logout}
         className="mt-10 cursor-pointer w-6 h-6"
